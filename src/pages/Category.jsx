f9ecbec5715941f0f579bf4f3e02b014a837c7c3
Sam Redmond <sam@fingermarkglobal.com>
2023-05-11 17:21:22 +1200

    Add serves filter

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -9,11 +9,26 @@ const Category = () => {
 
     const { name } = useParams();
     const [ categoryRecipes, setCategoryRecipes ] = useState([]);
+    const [ selectedServes, setSelectedServes] = useState("*");
 
     useEffect(() => {
 
         setCategoryRecipes(recipes[name.toLowerCase()].hits);
     }, [ name ]);
+
+    /**
+     * Filter recipes by the 'yield' or serve count
+     */
+    const servesFilter = (recipe) => {
+        switch (selectedServes) {
+            case '*':
+                return true;
+            case '+6':
+                return recipe.yield >= 6;
+            default:
+                return recipe.yield === parseInt(selectedServes)
+        }
+    }
 
 	return (
 		<IonPage>
@@ -24,6 +39,18 @@ const Category = () => {
                     </IonButtons>
 					<IonTitle>{ name } Recipes</IonTitle>
 				</IonToolbar>
+                <select value={selectedServes}
+                    onChange={(event) => {
+                        setSelectedServes(event.target.value);
+                }}>
+                    <option value="*">All</option>
+                    <option value="1">1 serves</option>
+                    <option value="2">2 serves</option>
+                    <option value="3">3 serves</option>
+                    <option value="4">4 serves</option>
+                    <option value="5">5 serves</option>
+                    <option value="6+">6+ serves</option>
+                </select>
 			</IonHeader>
 			<IonContent fullscreen>
 				<IonHeader collapse="condense">
@@ -33,12 +60,25 @@ const Category = () => {
 				</IonHeader>
 
                 <IonList>
-                    { categoryRecipes.map((categoryRecipe, index) => {
-                        const { recipe } = categoryRecipe;
-                        return (
-                            <RecipeListItem recipe={ recipe } key={ `recipe_${ index }` } />
+                {
+                    (() => {
+                        const filteredRecipes = categoryRecipes.filter(data => {
+                            return servesFilter(data.recipe); /* && another filter */
+                        });
+
+                        return filteredRecipes.length === 0 ? (
+                            <div>
+                                <h3>No results found.</h3>
+                                <p>Refine your search to see more results.</p>
+                            </div>
+                        ) : (
+                            filteredRecipes.map((categoryRecipe, index) => {
+                            const { recipe } = categoryRecipe;
+                            return <RecipeListItem recipe={recipe} key={`recipe_${index}`} />;
+                            })
                         );
-                    })}
+                    })()
+                }
                 </IonList>
 			</IonContent>
 		</IonPage>
